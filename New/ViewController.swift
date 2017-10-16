c493070fa5ebd8f7ba7cb82e61718cd203488eb5
Daniel Adams <dan@bowstreet.de>
2017-10-16 08:56:59 +0200

    Add new project

---
+++ New/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  New
+//
+//  Created by Daniel Adams on 16.10.17.
+//  Copyright © 2017 Bowstreet GmbH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
